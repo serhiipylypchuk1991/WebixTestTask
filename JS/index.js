5db79efdc82b859edd47ccaa95e23ee29556c586
Serhii <lingvopoligraf@yandex.by>
2021-01-01 20:08:03 +0300

    Master branch has fully edited

--- JS/index.js
+++ JS/index.js
@@ -2,23 +2,22 @@
 webix.ready(function(){
 
   var grid_data = [
-    { id:1, title:"The Shawshank Redemption", year:1994, votes:678790, rating:9.2, rank:1},
-    { id:2, title:"The Godfather", year:1972, votes:511495, rating:9.2, rank:2},
-    { id:3, title:"The Godfather: Part II", year:1974, votes:319352, rating:9.0, rank:3},
-    { id:4, title:"The Good, the Bad and the Ugly", year:1966, votes:213030, rating:8.9, rank:4},
-    { id:5, title:"My Fair Lady", year:1964, votes:533848, rating:8.9, rank:5},
-    { id:6, title:"12 Angry Men", year:1957, votes:164558, rating:8.9, rank:6}
+    {id:1, title:"The Shawshank Redemption", year:1994, votes:678790, rating:9.2, rank:1},
+    {id:2, title:"The Godfather", year:1972, votes:511495, rating:9.2, rank:2},
+    {id:3, title:"The Godfather: Part II", year:1974, votes:319352, rating:9.0, rank:3},
+    {id:4, title:"The Good, the Bad and the Ugly", year:1966, votes:213030, rating:8.9, rank:4},
+    {id:5, title:"My Fair Lady", year:1964, votes:533848, rating:8.9, rank:5},
+    {id:6, title:"12 Angry Men", year:1957, votes:164558, rating:8.9, rank:6}
   ];
 
   webix.ui({
 
     rows:[
-
         {
           view:"toolbar",
           css:"webix_dark",
           elements:[
-            { view: "label", label:"My App"},
+            {view: "label", label:"My App"},
             {
                view:"button",
                type:"icon",
@@ -42,10 +41,10 @@ webix.ready(function(){
                   css:"GrayBackground",
                   template:"#title#",
                   data:[
-                    { id:1, title:"Dashboard"},
-                    { id:2, title:"Users"},
-                    { id:3, title:"Products"},
-                    { id:4, title:"Locations"}
+                    {id:1, title:"Dashboard"},
+                    {id:2, title:"Users"},
+                    {id:3, title:"Products"},
+                    {id:4, title:"Locations"}
                   ]
                 },
                 {
@@ -69,19 +68,20 @@ webix.ready(function(){
               width:250,
               elements:[
                 {
+                  margin:10,//margin for all elements of rows[]
                   rows:[
-                    { template:"EDIT FILMS", type:"section"},
-                    { view:"text", label:"Title"},
-                    { view:"text", label:"Year" },
-                    { view:"text", label:"Rating" },
-                    { view:"text", label:"Votes" },
-                  ], margin:10
+                    {template:"EDIT FILMS", type:"section"},
+                    {view:"text", label:"Title"},
+                    {view:"text", label:"Year"},
+                    {view:"text", label:"Rating"},
+                    {view:"text", label:"Votes"},
+                  ]
                 },
                 {
-                    cols:[
-                    { view:"button", id:"btn_add_new", value:"Add new", css:"webix_primary"},
-                    {width:20},
-                    { view:"button", id:"btn_clear", value:"Clear", css:"webix_secondary"},
+                  margin:20,
+                  cols:[
+                    {view:"button", id:"btn_add_new", value:"Add new", css:"webix_primary"},
+                    {view:"button", id:"btn_clear", value:"Clear", css:"webix_secondary"},
                   ]
                 },{},{}
               ]
@@ -94,7 +94,6 @@ webix.ready(function(){
            height:30,
            width:550
         }
-
       ]
   });
 
